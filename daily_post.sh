a64dc7e31b1866b215d533569dfb53d7e43d70aa
kyawphyonaing <kyawphyonaing@aucfan.com>
2016-03-24 17:20:35 +0900

    change domain

--- daily_post.sh
+++ daily_post.sh
@@ -7,16 +7,16 @@ if [ $holiday = 'else' ] ; then
 
 #php /vagrant/work/google-api/index.php
 
-#192.168.101.36
-#php /deploy/qiita/google-api/index.php | php /deploy/qiita/google-api/json.php > /deploy/qiita/google-api/hogehoge.json
-#curl -XPOST -H 'Authorization: Bearer 796ddbc73e10ee1f1e64c4f621a0cfff1839bcec' -H 'Content-Type: application/json' https://afdsol.qiita.com/api/v2/items/ -d @/deploy/qiita/google-api/hogehoge.json
+#192.168.101.195
+php /deploy/qiita/googleapi/index.php | php /deploy/qiita/googleapi/json.php > /deploy/qiita/googleapi/hogehoge.json
+curl -XPOST -H 'Authorization: Bearer 796ddbc73e10ee1f1e64c4f621a0cfff1839bcec' -H 'Content-Type: application/json' https://afdsol.qiita.com/api/v2/items/ -d @/deploy/qiita/googleapi/hogehoge.json
 
 #vagrant test
-#php /vagrant/work/google-api/index.php | php /vagrant/work/google-api/json.php > /vagrant/work/google-api/hogehoge.json
+#php /vagrant/work/google-api/index.php | php /vagrant/work/googleapi/json.php > /vagrant/work/googleapi/hogehoge.json
 #curl -XPOST -H 'Authorization: Bearer 796ddbc73e10ee1f1e64c4f621a0cfff1839bcec' -H 'Content-Type: application/json' https://afdsol.qiita.com/api/v2/items/ -d @/vagrant/work/google-api/hogehoge.json
 
 #local test
-php /Users/kyawphyonaing/Desktop/google-api/index.php | php /Users/kyawphyonaing/Desktop/google-api/json.php > /Users/kyawphyonaing/Desktop/google-api/hogehoge.json
-curl -XPOST -H 'Authorization: Bearer 796ddbc73e10ee1f1e64c4f621a0cfff1839bcec' -H 'Content-Type: application/json' https://afdsol.qiita.com/api/v2/items/ -d @/Users/kyawphyonaing/Desktop/google-api/hogehoge.json
+#php /Users/kyawphyonaing/Desktop/google-api/index.php | php /Users/kyawphyonaing/Desktop/googleapi/json.php > /Users/kyawphyonaing/Desktop/googleapi/hogehoge.json
+#curl -XPOST -H 'Authorization: Bearer 796ddbc73e10ee1f1e64c4f621a0cfff1839bcec' -H 'Content-Type: application/json' https://afdsol.qiita.com/api/v2/items/ -d @/Users/kyawphyonaing/Desktop/googleapi/hogehoge.json
 
 fi
--- mail.php
+++ mail.php
@@ -1,7 +1,8 @@
 <?php
 //$to      = 'memo@aucfan.com';
 $to 	 = 'kyawphyo85@gmail.com';
-$subject = date("Ymd") . mb_encode_mimeheader(" 開発G作業予定");
+//$subject = date("Ymd") . mb_encode_mimeheader(" 開発G作業予定");
+$subject = date("Ymd") . " 開発G作業予定";
 $message = file_get_contents('/deploy/qiita/googleapi/message.txt', true);
 $headers = 'From: kyawphyonaing@aucfan.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
